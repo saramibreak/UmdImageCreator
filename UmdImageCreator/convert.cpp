c70501b7d0054296200443cea20a55512d6d7ce7
saramibreak <upswbsbj_20150526@yahoo.co.jp>
2023-10-12 23:02:17 +0900

    Updated copyright year

--- UmdImageCreator/convert.cpp
+++ UmdImageCreator/convert.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018-2021 sarami
+ * Copyright 2018-2023 sarami
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
