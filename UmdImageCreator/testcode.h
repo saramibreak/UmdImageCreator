8975343289e032e41d4bb26bffd855ca7ae8fa54
saramibreak <upswbsbj_20150526@yahoo.co.jp>
2024-01-04 23:05:20 +0900

    updated copyright year

--- UmdImageCreator/testcode.h
+++ UmdImageCreator/testcode.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018-2022 sarami
+ * Copyright 2018-2024 sarami
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
